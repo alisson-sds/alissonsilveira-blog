5b8a5c64b6fc934a74307ce68734756e8938cf40
Alisson Silveira <alisson-sds@hotmail.com>
2025-12-05 09:37:46 -0300

    Foto e quadro de tecnologias

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { CiLinkedin, CiYoutube } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
+import foto from './assets/foto.jpg';
 
 function App() {
 
@@ -22,33 +23,57 @@ function App() {
         </div>
       </nav>
       <div className="p-4 flex w-full">
-        <div className=" border-2 border-red-500">
-          Minha foto
+        <div className=" border-2 border-red-500 w-1/3">
+          <img src={foto}></img>
         </div>
-        <div className=" border-2 border-red-500 w-full">
-          <div className="flex">
+        <div className="flex flex-col gap-2 border-2 border-red-500 w-full">
+          <div className="flex justify-between">
             <div>
               <p className="">
                 Sobre mim <br /> siofasdfuhasdifuo <br /> asduifhasidoufhsdaiuoh <br /> asduifhasidoufhsdaiuoh <br /> asduifhasidoufhsdaiuoh
               </p>
             </div>
-            <div>
-              <p>
-                Progress <br /> Python <br /> Java <br /> React <br /> Next
-              </p>
+            <div className="flex flex-col text-gray-300 gap-2 select-none">
+              <div className="flex gap-2 w-full">
+                <p className="text-6xl border border-gray-300 cursor-pointer">
+                  Progress
+                </p>
+                <div className="flex flex-col items-start justify-center text-2xl gap-2 w-full">
+                  <p className="border border-gray-300 w-full p-1 cursor-pointer">Next.js</p>
+                  <p className="border border-gray-300 w-full p-1 cursor-pointer">Tailwind CSS</p>
+                </div>
+              </div>
+              <div className="flex w-full gap-2">
+                <div className="flex flex-col items-start justify-center text-2xl gap-2">
+                  <p className="border border-gray-300 w-full p-1 cursor-pointer">Java</p>
+                  <p className="border border-gray-300 w-full p-1 cursor-pointer">React</p>
+                </div>
+                <p className="text-6xl border border-gray-300 cursor-pointer">Python</p>
+                <div className="flex flex-col items-start justify-center text-2xl gap-2">
+                  <p className="border border-gray-300 w-full p-1 cursor-pointer">FastAPI</p>
+                  <p className="border border-gray-300 w-full p-1 cursor-pointer">PostgreSQL</p>
+                </div>
+              </div>
+              <div className="flex gap-2 w-full ">
+                <div className="flex flex-col items-start justify-center text-2xl gap-2 w-full">
+                  <p className="border border-gray-300 w-full p-1 cursor-pointer">Typescript</p>
+                  <p className="border border-gray-300 w-full p-1 cursor-pointer">Data Science</p>
+                </div>
+                <p className="text-6xl border border-gray-300 cursor-pointer">Datasul</p>
+              </div>
             </div>
           </div>
-          <div className='flex justify-center gap-4 text-gray-500 text-2xl'>
+          <div className='flex justify-center gap-16 text-gray-300 text-2xl'>
             <a href="https://www.linkedin.com/in/alisson-sds/" target="_blank" className='flex text-center justify-center gap-2 items-center border-2 p-2 font-inter'>
-              <CiLinkedin className='text-gray-500' />
+              <CiLinkedin />
               LinkedIn
             </a>
             <a href="https://www.youtube.com/@Alisson-Sds" target="_blank" className='flex text-center justify-center gap-2 items-center border-2 p-2 font-inter'>
-              <CiYoutube className='text-gray-500' />
+              <CiYoutube  />
               YouTube
             </a>
             <a href="https://github.com/alisson-sds" target="_blank" className='flex text-center justify-center gap-2 items-center border-2 p-2 font-inter'>
-              <FaGithub className='text-gray-500' />
+              <FaGithub  />
               Github
             </a>
           </div>
