9a641f2fb9ad40afd319b00b82f64e924ece8e5e
Alisson Silveira <alisson-sds@hotmail.com>
2025-12-10 08:55:25 -0300

    Css homepage

--- src/App.tsx
+++ src/App.tsx
@@ -36,32 +36,33 @@ function App() {
   const TEXT_HOVER_COLOR = 'hover:text-white';
   const BASE_BORDER_CLASS = 'border-2 cursor-pointer transition-all duration-200';
   const BASE_TEXT_CLASS = 'text-gray-400 select-none';
+  const BASIC_SHADOW = 'shadow-lg';
 
   return (
     <div className="min-h-screen bg-gray-950"> 
       <nav className="pt-4 pb-4 select-none">
         <div className={`flex justify-center gap-16 text-gray-400 text-2xl`}>
-          <a href="" target="_blank" className={`flex text-center justify-center font-inter gap-2 items-center ${TEXT_HOVER_COLOR} transition delay-100 `}>
+          <a href="" target="" className={`flex text-center justify-center font-inter gap-2 items-center ${TEXT_HOVER_COLOR} transition delay-100 `}>
             Home
           </a>
-          <a href="" target="_blank" className={`flex text-center justify-center font-inter gap-2 items-center ${TEXT_HOVER_COLOR} transition delay-100`}>
+          <a href="" target="" className={`flex text-center justify-center font-inter gap-2 items-center ${TEXT_HOVER_COLOR} transition delay-100`}>
             Artigos
           </a>
-          <a href="" target="_blank" className={`flex text-center justify-center font-inter gap-2 items-center ${TEXT_HOVER_COLOR} transition delay-100`}>
+          <a href="" target="" className={`flex text-center justify-center font-inter gap-2 items-center ${TEXT_HOVER_COLOR} transition delay-100`}>
             Projetos
           </a>
-          <a href="" target="_blank" className={`flex text-center justify-center font-inter gap-2 items-center ${TEXT_HOVER_COLOR} transition delay-100`}>
+          <a href="" target="" className={`flex text-center justify-center font-inter gap-2 items-center ${TEXT_HOVER_COLOR} transition delay-100`}>
             Sobre mim
           </a>
         </div>
       </nav>
       <div className="p-8 flex w-full">
-        <div className="w-1/3 border select-none">
-          <img src={foto} className="w-full h-full object-cover rounded-lg shadow-2xl"></img> 
+        <div className="w-1/3 select-none">
+          <img src={foto} className={`w-full h-full object-cover rounded-lg ${BASIC_SHADOW}`}></img> 
         </div>
         <div className="flex flex-col gap-4 w-full pl-8"> 
-          <div className="flex justify-between">
-            <div className="w-full p-2">
+          <div className="flex justify-between gap-2">
+            <div className="w-full bg-gray-900  p-4 rounded-lg ${BASIC_SHADOW} ">
               <div className="flex gap-2 mb-4">
                 <BR
                   title="Brasil"
@@ -74,55 +75,55 @@ function App() {
                   onClick={() => handleLanguageChange('US')}
                 />
               </div>
-              <p className={`font-inter ${TEXT_COLOR} select-none bg-gray-900 p-4 rounded-lg shadow-inner`}> 
+              <p className={`font-inter ${TEXT_COLOR} select-none `}> 
                 {currentText.text}
               </p>
             </div>
 
-            <div className={`flex flex-col ${BASE_TEXT_CLASS} gap-2 select-none p-2`}>
+            <div className={`flex flex-col ${BASE_TEXT_CLASS} gap-2 select-none `}>
               <div className="flex gap-2 w-full">
-                <p className={`text-6xl ${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} p-1`}>
+                <p className={`text-6xl ${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} ${BASIC_SHADOW} p-1`}>
                   Progress
                 </p>
                 <div className="flex flex-col items-start justify-center text-2xl gap-2 w-full">
-                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} w-full p-1`}>Next.js</p>
-                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} w-full p-1`}>Tailwind CSS</p>
+                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} ${BASIC_SHADOW} w-full p-1`}>Next.js</p>
+                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} ${BASIC_SHADOW} w-full p-1`}>Tailwind CSS</p>
                 </div>
               </div>
               <div className="flex w-full gap-2">
                 <div className="flex flex-col items-start justify-center text-2xl gap-2">
-                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} w-full p-1`}>Java</p>
-                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} w-full p-1`}>React</p>
+                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} ${BASIC_SHADOW} w-full p-1`}>Java</p>
+                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} ${BASIC_SHADOW} w-full p-1`}>React</p>
                 </div>
-                <p className={`text-6xl ${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} p-1`}>Python</p>
+                <p className={`text-6xl ${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} ${BASIC_SHADOW} p-1`}>Python</p>
                 <div className="flex flex-col items-start justify-center text-2xl gap-2">
-                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} w-full p-1`}>FastAPI</p>
-                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} w-full p-1`}>PostgreSQL</p>
+                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} ${BASIC_SHADOW} w-full p-1`}>FastAPI</p>
+                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} ${BASIC_SHADOW} w-full p-1`}>PostgreSQL</p>
                 </div>
               </div>
               <div className="flex gap-2 w-full ">
                 <div className="flex flex-col items-start justify-center text-2xl gap-2 w-full">
-                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} w-full p-1`}>Typescript</p>
-                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} w-full p-1`}>Data Science</p>
+                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} ${BASIC_SHADOW} w-full p-1`}>Typescript</p>
+                  <p className={`${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} ${BASIC_SHADOW} w-full p-1`}>Data Science</p>
                 </div>
-                <p className={`text-6xl ${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} p-1`}>Datasul</p>
+                <p className={`text-6xl ${INACTIVE_BORDER} ${BASE_BORDER_CLASS} ${TEXT_HOVER_COLOR} ${BASIC_SHADOW} p-1`}>Datasul</p>
               </div>
             </div>
           </div>
           
           <div className={`flex justify-center gap-16 ${TEXT_COLOR} text-2xl select-none`}>
             <a href="https://www.linkedin.com/in/alisson-sds/" target="_blank" 
-               className={`flex text-center justify-center gap-2 items-center ${INACTIVE_BORDER} ${BASE_BORDER_CLASS} p-2 font-inter ${TEXT_HOVER_COLOR} hover:${ACTIVE_COLOR}`}>
+               className={`flex text-center justify-center gap-2 items-center ${INACTIVE_BORDER} ${BASE_BORDER_CLASS} p-2 font-inter ${BASIC_SHADOW} ${TEXT_HOVER_COLOR} hover:${ACTIVE_COLOR}`}>
               <CiLinkedin />
               LinkedIn
             </a>
             <a href="https://www.youtube.com/@Alisson-Sds" target="_blank" 
-               className={`flex text-center justify-center gap-2 items-center ${INACTIVE_BORDER} ${BASE_BORDER_CLASS} p-2 font-inter ${TEXT_HOVER_COLOR} hover:${ACTIVE_COLOR}`}>
+               className={`flex text-center justify-center gap-2 items-center ${INACTIVE_BORDER} ${BASE_BORDER_CLASS} p-2 font-inter ${BASIC_SHADOW} ${TEXT_HOVER_COLOR} hover:${ACTIVE_COLOR}`}>
               <CiYoutube />
               YouTube
             </a>
             <a href="https://github.com/alisson-sds" target="_blank" 
-               className={`flex text-center justify-center gap-2 items-center ${INACTIVE_BORDER} ${BASE_BORDER_CLASS} p-2 font-inter ${TEXT_HOVER_COLOR} hover:${ACTIVE_COLOR}`}>
+               className={`flex text-center justify-center gap-2 items-center ${INACTIVE_BORDER} ${BASE_BORDER_CLASS} p-2 font-inter ${BASIC_SHADOW} ${TEXT_HOVER_COLOR} hover:${ACTIVE_COLOR}`}>
               <FaGithub />
               Github
             </a>
